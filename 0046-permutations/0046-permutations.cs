ad502221f9df1ee88093b5ab34edf2dbe8b66364
Souvik Ghosh <g.souvik04@gmail.com>
2025-01-18 16:14:38 +0530

    Time: 1 ms (87.49%), Space: 47.7 MB (14.93%) - LeetHub

--- 0046-permutations/0046-permutations.cs
+++ 0046-permutations/0046-permutations.cs
@@ -2,22 +2,18 @@ public class Solution {
     public IList<IList<int>> Permute(int[] nums) {
         IList<IList<int>> output = new List<IList<int>>();
         Backtrack(nums, output, new List<int>());
+
         return output;
     }
 
     private void Backtrack(int[] nums, IList<IList<int>> output, List<int> temp){
-        // Goal
         if(temp.Count == nums.Length){
-            output.Add(new List<int>(temp));
+            output.Add(temp.ToList());
+            return;
         }
-        else{
-            // Choice
-            for(int i = 0; i < nums.Length; i++){
-                // Constraint
-                if(temp.Contains(nums[i])){
-                    continue;
-                }
 
+        for(int i = 0; i < nums.Length; i++){
+            if(!temp.Contains(nums[i])){
                 temp.Add(nums[i]);
                 Backtrack(nums, output, temp);
                 temp.RemoveAt(temp.Count - 1);
@@ -25,3 +21,19 @@ public class Solution {
         }
     }
 }
+
+/*
+
+1. initialize IList<IList<int>> output
+2. perform backtrack(nums, output, temp)
+    - if temp.length == nums.length, add temp to output and return
+    - iterate over nums from 0 to n - 1
+    - add to temp and backtrack
+3. return output
+
+-----
+
+Time complexity: O(n * n!)
+Space complexity: O(n * n!)
+
+*/
