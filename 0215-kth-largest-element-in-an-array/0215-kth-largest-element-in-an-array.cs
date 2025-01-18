b3157208e5a49b0f12f39d83e66b26490201dfa8
Souvik Ghosh <g.souvik04@gmail.com>
2025-01-18 15:45:22 +0530

    Time: 155 ms (47.36%), Space: 56.3 MB (59.59%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -1,21 +1,27 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k) {
-        /*
-            1. Create a priority queue (min heap)
-            2. iterate over the input array and insert each element in PQ
-            3. during insertion check, if PQ count == k and top element value less than current element (array[i]) then dequeue the element
-            4. return PQ.Peek()
-        */
-
         PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
 
-        foreach (int n in nums) {
+        foreach(int n in nums){
             minHeap.Enqueue(n, n);
-            if (minHeap.Count > k) {
+
+            if(minHeap.Count > k){
                 minHeap.Dequeue();
             }
         }
 
-        return minHeap.Peek();
+        return minHeap.Dequeue();
     }
 }
+
+/*
+
+1. create a min-heap to store the nums
+2. iterate over nums and enqueue them in min-heap
+3. if queue count > k dequeue
+4. return queue.dequeue();
+
+Time complexity: O(logn)
+Space complexity: O(n)
+
+*/
