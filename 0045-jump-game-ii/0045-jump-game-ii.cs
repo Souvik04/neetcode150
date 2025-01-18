3be6d5244d480093139b900f062c76489d4c8e85
Souvik Ghosh <g.souvik04@gmail.com>
2025-01-18 07:12:58 +0530

    Time: 0 ms (100%), Space: 45.7 MB (26.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cs
+++ 0045-jump-game-ii/0045-jump-game-ii.cs
@@ -1,32 +1,33 @@
 public class Solution {
     public int Jump(int[] nums) {
-        if (nums.Length <= 1) return 0; // No jumps needed for 0 or 1 element
+        int jumps = 0;
+        int currentMax = 0;
+        int goal = 0;
 
-        int jumps = 0; // Number of jumps made
-        int currentEnd = 0; // Farthest index that can be reached with the current number of jumps
-        int farthest = 0; // Farthest index that can be reached with the next jump
+        for(int i = 0; i < nums.Length - 1; i++){
+            currentMax = Math.Max(currentMax, i + nums[i]);
 
-        for (int i = 0; i < nums.Length - 1; i++) {
-            // Update the farthest index we can reach
-            farthest = Math.Max(farthest, i + nums[i]);
-
-            // If we have come to the end of the current jump range
-            if (i == currentEnd) {
-                jumps++; // We need to make a jump
-                currentEnd = farthest; // Update the end for the next jump
-
-                // If we can reach or exceed the last index
-                if (currentEnd >= nums.Length - 1) {
-                    break;
-                }
+            if(i == goal){
+                jumps++;
+                goal = currentMax;            
             }
+
+            if(goal >= nums.Length - 1){
+                break;
+            }                
         }
 
-        return jumps; // Return the total number of jumps made
+        return jumps;
     }
 }
 
 /*
+
+1. initialize jumps = 0, goal = 0, currentMax = 0
+2. iterate over nums and set currentMax = max(currentMax, i + nums[i]) as farthest jump from 0 to n - 1
+3. check if i == goal, increment jumps by 1 and set goal = currentMax
+4. if goal >= n - 1, break as we reach end
+4. return jumps as output
 
 Time complexity: O(n)
 Space complexity: O(1)
