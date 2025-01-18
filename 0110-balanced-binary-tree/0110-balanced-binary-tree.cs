639a448319e749b0692fb54044ae6c81f96f7a66
Souvik Ghosh <g.souvik04@gmail.com>
2025-01-18 15:26:49 +0530

    Time: 0 ms (100%), Space: 46.4 MB (68.79%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cs
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cs
@@ -13,20 +13,36 @@
  */
 public class Solution {
     public bool IsBalanced(TreeNode root) {
-        if(root == null)
+        if(root == null){
             return true;
+        }
 
         int left = GetHeight(root.left);
         int right = GetHeight(root.right);
-        return Math.Abs(left - right) <= 1 && IsBalanced(root.left) && IsBalanced(root.right);
+
+        return Math.Abs(left - right ) <= 1 && IsBalanced(root.left) && IsBalanced(root.right);
     }
 
-    private int GetHeight(TreeNode root){
-        if(root == null)
+    private int GetHeight(TreeNode node){
+        if(node == null){
             return 0;
+        }
 
-        int left = GetHeight(root.left) + 1;
-        int right = GetHeight(root.right) + 1;
+        int left = GetHeight(node.left) + 1;
+        int right = GetHeight(node.right) + 1;
+
         return Math.Max(left, right);
     }
 }
+
+/*
+
+1. get height of left tree and right tree recursively in getHeight(node)
+2. check and return left <= right && IsBalanced(node.left) && IsBalanced(node.right) <= 1
+
+*Height of tree: calculate from leaf to root
+
+Time complexity: O(h)
+Space complexity: O(h)
+
+*/
