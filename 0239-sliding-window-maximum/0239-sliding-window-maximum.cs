f65906443c6aaf6a7f093adbc22e1562ecf7afdd
Souvik Ghosh <g.souvik04@gmail.com>
2025-01-23 19:38:07 +0530

    Time: 43 ms (48.28%), Space: 147.8 MB (5.04%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cs
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cs
@@ -1,24 +1,28 @@
 public class Solution {
     public int[] MaxSlidingWindow(int[] nums, int k) {
         List<int> output = new List<int>();
-        LinkedList<int> linkedList = new LinkedList<int>();
-
+        LinkedList<int> list = new LinkedList<int>();
+        
         for(int i = 0; i < nums.Length; i++){
-            if(linkedList.Count > 0 && linkedList.First.Value == i - k){
-                linkedList.RemoveFirst();
+            // remove indices outside the current window from front
+            if(list.Count > 0 && list.First.Value < i - k + 1){
+                list.RemoveFirst();
             }
-
-            while(linkedList.Count > 0 && nums[linkedList.Last.Value] <= nums[i]){
-                linkedList.RemoveLast();
+            
+            // maintain decreasing order by removing smaller elements from the back
+            while(list.Count > 0 && nums[list.Last.Value] <= nums[i]){
+                list.RemoveLast();
             }
-
-            linkedList.AddLast(i);
-
+            
+            // add the current index into the back
+            list.AddLast(i);
+            
+            // add maximum for the current window to output
             if(i >= k - 1){
-                output.Add(nums[linkedList.First.Value]);
+                output.Add(nums[list.First.Value]);
             }
         }
-
+        
         return output.ToArray();
     }
 }
